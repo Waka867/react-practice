0cd6b441e06b7169e63488e688dea838192b1f51
Ledwing Hernandez <ledwing.hernandez@gmail.com>
2020-01-21 16:21:52 -0500

    Added new react app named project react for practice Left off from video https://youtu.be/dGcsHMXbSOA?t=835

---
+++ projectreact/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+function App() {
+  return(
+    <div>
+      <h1>Hello React!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+//
+// function AppB() {
+//   return(
+//     <div>
+//       <h3>This is the subtitle</h3>
+//       <button>Test</button>
+//     </div>
+//   );
+// }
+
+// // export default AppB;
---
+++ readme.md
@@ -0,0 +1,3 @@
+React practice by Ledwing Hernandez
+
+Following tutorial at https://www.youtube.com/watch?v=dGcsHMXbSOA for projectreact directory
